089e1f8c213ce6810fb20d6ebec41ded758dcdd5
Nermeen Abdel Azeem <nermeen.abdel.azeem810@gmail.com>
2022-01-05 13:02:16 +0200

    setup_navigation_component

--- app/src/main/java/com/rino/visualdestortion/MainActivity.kt
+++ app/src/main/java/com/rino/visualdestortion/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rino.visualdestortion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
+    private fun splashSetup(navController: NavController){
+        CoroutineScope(Dispatchers.Default).launch{
+            delay(3000)
+            CoroutineScope(Dispatchers.Main).launch{
+                navController.popBackStack()
+                navController.navigate(R.id.shopFragment)
+            }
+        }
+    }
+
 }
