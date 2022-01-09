059a683420a27086be459fcb6550017551a6d4ba
Nermeen Abdel Azeem <nermeen.abdel.azeem810@gmail.com>
2022-01-09 17:59:03 +0200

    login&splash&welcome screens design

--- app/src/main/java/com/rino/visualdestortion/MainActivity.kt
+++ app/src/main/java/com/rino/visualdestortion/MainActivity.kt
@@ -2,7 +2,12 @@ package com.rino.visualdestortion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-
+        navController.navigate(R.id.splashFragment)
     }
 
 
