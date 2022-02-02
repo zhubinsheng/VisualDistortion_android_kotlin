3959ef74fd3e23e948fe75f1bcddbbd848a54215
AndoridDEV <nermeen.abdel.azeem810@gmail.com>
2022-02-02 06:58:10 -0800

    fingerPrint Athu

--- app/src/main/java/com/rino/visualdestortion/ui/home/MainActivity.kt
+++ app/src/main/java/com/rino/visualdestortion/ui/home/MainActivity.kt
@@ -3,6 +3,8 @@ package com.rino.visualdestortion.ui.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
@@ -11,6 +13,9 @@ import com.rino.visualdestortion.ui.services.ServicesFragment
 
 class MainActivity : AppCompatActivity() {
     lateinit var bottomNavigation : MeowBottomNavigation
+    lateinit var handler: Handler
+    lateinit var runnable: Runnable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +25,30 @@ class MainActivity : AppCompatActivity() {
        // navController.navigate(R.id.splashFragment)
         bottomNavigation = findViewById(R.id.meowBottomNavigation)
         navigationSetup(navController,bottomNavigation)
+        handler = Handler()
+        runnable = Runnable {
+            Toast.makeText(this, "the session ended please login again", Toast.LENGTH_SHORT).show()
+            navController.popBackStack()
+            navController.navigate(R.id.fingerPrintFragment)
+        }
+        startHandler()
+    }
+    override fun onUserInteraction() {
+        // TODO Auto-generated method stub
+        super.onUserInteraction()
+        stopHandler()
+        startHandler()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        stopHandler()
+    }
+    private fun stopHandler() {
+        handler.removeCallbacks(runnable)
+    }
+    private fun startHandler() {
+        handler.postDelayed(runnable, 10000.toLong())
     }
     private fun navigationSetup(navController: NavController, bottomNavigation: MeowBottomNavigation){
         bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24))
--- app/src/main/java/com/rino/visualdestortion/ui/services/ServicesFragment.kt
+++ app/src/main/java/com/rino/visualdestortion/ui/services/ServicesFragment.kt
@@ -1,10 +1,12 @@
 package com.rino.visualdestortion.ui.services
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
@@ -21,10 +23,12 @@ class ServicesFragment : Fragment() {
     private lateinit var serviceAdapter: ServiceAdapter
     private lateinit var servicesList: List<ServiceTypes>
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
